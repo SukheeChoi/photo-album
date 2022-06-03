7a269d8538035e14e8e8840ecd44500ab8a9ec28
Shin-HyunJu <afww@daum.net>
2022-06-03 16:40:00 +0900

    list vue 수정중

--- src/apis/board.js
+++ src/apis/board.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 //게시물 목록 요청
-async function getBoardList(pageNo=1) {
+async function getBoardList(pageNo) {
   let response = null;
   try {
-    response = await axios.get("/board/list", pageNo);
-    return response.data;
+    response = await axios.get("/board2/list", {params:{pageNo}}); //queryString 방식으로 넘기기, 이름이 같으면 생략 가능
+
+    return {result:"success", data:response.data};
   } catch (error) {
     console.log(error);
+    console.log("실패!");
   }
 }
 
