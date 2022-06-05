8afc47a47e100656437ee610d4b13b8ed31ed176
lee0507 <2eastbow@gmail.com>
2022-06-05 18:45:01 +0900

    read.vue 추가

--- src/apis/board.js
+++ src/apis/board.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-import apiAuth from '@/apis/auth';
+import apiAuth from "@/apis/auth";
 
 //게시물 목록 요청
 async function getBoardList(pageNo) {
   let response = null;
   try {
-    response = await axios.get("/board2/list", {params:{pageNo}}); //queryString 방식으로 넘기기, 이름이 같으면 생략 가능
+    response = await axios.get("/board2/list", { params: { pageNo } }); //queryString 방식으로 넘기기, 이름이 같으면 생략 가능
 
-    return {result:"success", data:response.data};
+    return { result: "success", data: response.data };
   } catch (error) {
     console.log(error);
     console.log("실패!");
@@ -18,15 +18,15 @@ async function getBoardList(pageNo) {
 async function createBoard(multipartFormData) {
   let dbBoard = null;
   try {
-    const response = await axios.post('/board2/', multipartFormData);
+    const response = await axios.post("/board2/", multipartFormData);
     dbBoard = response.data;
-  } catch(error) {
-    if(error.response) {
+  } catch (error) {
+    if (error.response) {
       // 서버 응답이 있다
-      if(error.response.status === 403) {
-        if(await apiAuth.refreshToken()) {
-          console.log('~~~~~~~~~~~~~~~~');
-          const response = await axios.post('/board2/', multipartFormData);
+      if (error.response.status === 403) {
+        if (await apiAuth.refreshToken()) {
+          console.log("~~~~~~~~~~~~~~~~");
+          const response = await axios.post("/board2/", multipartFormData);
           dbBoard = response.data;
         }
       }
@@ -37,7 +37,65 @@ async function createBoard(multipartFormData) {
   return dbBoard;
 }
 
-export default {
-  getBoardList
-  , createBoard
+//게시물 1개 요청
+async function getBoard(bno, hit) {
+  let response = null;
+  try {
+    response = await axios.get(`/board2/${bno}?hit=${hit}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+//이미지 정보 가져오기
+async function getImages(bno) {
+  let response = null;
+  try {
+    response = await axios.get(`/board2/images/${bno}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//이미지 하나 다운로드 요청
+async function downloadImage(ino) {
+  let response = null;
+  try {
+    response = await axios.get(`/board2/image/${ino}`, { responseType: "blob" });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//전체 이미지 가져오는 요청
+async function getImagesLee(bno) {
+  let response = null;
+  let result = null;
+  try {
+    response = getImages(bno).then(async (data) => {
+      result = await data.images.map(async (data) => {
+        let dataLee = await axios.get(`/board2/image/${data.ino}`, { responseType: "blob" });
+        return dataLee.data;
+      });
+
+      const lee = await Promise.all(result);
+      return lee;
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default {
+  getBoardList,
+  createBoard,
+  getBoard,
+  getImages,
+  downloadImage,
+  getImagesLee,
+};
